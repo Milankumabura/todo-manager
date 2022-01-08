e80b14cf623565e38d0cc04eb99e19cb819699d5
Milankumabura <milanbudha007.mgr@gmail.com>
2022-01-08 13:50:06 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import Header from "./MyComponents/Header";
+import {Todos} from "./MyComponents/Todos";
+import {Footer} from "./MyComponents/Footer";
+import React, {useState} from 'react';
+
+function App() {
+  const onDelete=(todo) => {
+    console.log('I am onDelete of todo',todo);
+    // Deleting this way in react Does not work
+    // let index = todos.indexOf(todo);
+    // todos.splice(index,1);
+    setTodos(todos.filter((e)=> {
+      return e!==todo;
+    
+    }))
+  }
+
+  
+  const [todos,setTodos] = useState([
+    {
+      sno: 1,
+      title: "Go to the market",
+      desc: "You need to go to the market to get this job done"
+    },
+    {
+      sno: 2,
+      title: "Go to the mall",
+      desc: "You need to go to the market to get this job done2"
+
+    },
+    {
+      sno: 3,
+      title: "Go to the ghat",
+      desc: "You need to go to the market to get this job done3"
+
+    },
+  ]);
+  return (
+    <>
+      <Header title="My Todos List" searchBar={true}/>
+      <Todos todos={todos} onDelete={onDelete}/>
+      <Footer/>
+  
+    </>
+  );
+}
+
+export default App;
