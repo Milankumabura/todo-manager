6b8ee3b3fa75dd5887b0ad674943352b1c287a6c
Milankumabura <milanbudha007.mgr@gmail.com>
2022-01-08 14:31:44 +0545

    footer

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-        <footer className='bg-dark text-light py-3'>
+        <footer className='bg-dark text-light py-3 footer-bottom'>
             <p className="text-center">
                 Copyright &copy; MyTodosList.com
+                
             </p>
             
         </footer>
